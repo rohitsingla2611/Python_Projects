94b7f372cfef09a092cead18391be6bc5450f13c
Rohit Singla <rohitsingla2611@gmail.com>
2018-07-06 09:50:05 +0530

    Update

--- All Files/Regular Expression/regular_expression1.py
+++ All Files/Regular Expression/regular_expression1.py
@@ -1,1 +1,18 @@
-pattern = " My M.No. is 9988899911"
+import re
+
+pat = "1111551222"
+pat1 = "[0-9]+"
+text = " My M.No. is 9988899911 is 1111551222"
+
+print('-----------------')
+print(re.search(pat, text))
+
+print('-----------------')
+if re.search(pat1, text):
+    print("Found")
+else:
+    print(" Not Found")
+
+print('-----------------')
+if re.search(pat1, text):
+    print(re.search(pat1, text))
