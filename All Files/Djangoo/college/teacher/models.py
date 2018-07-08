cc5057f83f98e515bb35e9322323f87905800186
Rohit Singla <rohitsingla2611@gmail.com>
2018-07-08 22:13:52 +0530

    Update

--- All Files/Djangoo/college/teacher/models.py
+++ All Files/Djangoo/college/teacher/models.py
@@ -8,3 +8,6 @@ class Teacher(models.Model):
     IdNo = models.CharField(max_length=8)
     Name = models.CharField(max_length=15)
     Department = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.IdNo + self.Name + self.Department
