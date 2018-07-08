87f4d5299cd3d061736b7c0c2c9a0245879a655d
Rohit Singla <rohitsingla2611@gmail.com>
2018-07-08 11:34:01 +0530

    Update

--- All Files/Djangoo/college/student/models.py
+++ All Files/Djangoo/college/student/models.py
@@ -2,7 +2,7 @@
 from django.db import models
 
 
-class Student(models.Models):
+class Student(models.Model):
     RollNo = models.CharField(max_length=8)
     Name = models.CharField(max_length=15)
     Course = models.CharField(max_length=10)
--- All Files/Djangoo/college/teacher/models.py
+++ All Files/Djangoo/college/teacher/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+# Create your models here
+
+
+class Teacher(models.Model):
+    IdNo = models.CharField(max_length=8)
+    Name = models.CharField(max_length=15)
+    Department = models.CharField(max_length=10)
