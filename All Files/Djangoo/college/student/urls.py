79d19a9ff672f68464e489aa6c64e723d5b8454a
Rohit Singla <rohitsingla2611@gmail.com>
2018-07-08 11:00:17 +0530

    Update

--- All Files/Djangoo/college/student/urls.py
+++ All Files/Djangoo/college/student/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('Student_front/', views.front_page),
-    path('student_data/', views.front_page)
+    path('Student/front/', views.front_page),
+    path('Student/data/', views.front_page)
 ]
--- All Files/Djangoo/college/teacher/urls.py
+++ All Files/Djangoo/college/teacher/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('Teacher_front/', views.front_page),
-    path('Teacher_data/', views.front_page)
+    path('Teacherfront/', views.front_page),
+    path('Teacherdata/', views.data())
 ]
