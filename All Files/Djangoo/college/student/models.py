69df576490083ea21f2873a9950cfe71abc2cb16
Rohit Singla <rohitsingla2611@gmail.com>
2018-07-08 11:56:36 +0530

    Update

--- All Files/Djangoo/college/student/models.py
+++ All Files/Djangoo/college/student/models.py
@@ -6,3 +6,6 @@ class Student(models.Model):
     RollNo = models.CharField(max_length=8)
     Name = models.CharField(max_length=15)
     Course = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.Rollno + self.Name + self.Course
