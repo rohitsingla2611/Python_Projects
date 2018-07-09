d40651aef90decc34e003762ad56eb1ef591227f
Rohit Singla <rohitsingla2611@gmail.com>
2018-07-09 14:13:35 +0530

    Update

--- All Files/Djangoo/college/classroom/models.py
+++ All Files/Djangoo/college/classroom/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Classroom(models.Model):
+    RoomNo = models.CharField(max_length=4)
+    Seats = models.CharField(max_length=4)
+    AllottedTo = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.RoomNo, self.Seats, self.AllottedTo
