9c786566b3a5354527e17869cd986589e98b9bb3
Rohit Singla <rohitsingla2611@gmail.com>
2018-07-10 10:08:05 +0530

    Update

--- All Files/Djangoo/college/student/views.py
+++ All Files/Djangoo/college/student/views.py
@@ -1,7 +1,7 @@
 # Create your views here.
 
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from .models import Student
 
 
@@ -16,29 +16,45 @@ def front_page(request):
 
 
 def data(request, roll_no):
+    std = get_object_or_404(Student, RollNo=roll_no)
+    html = "<ol>"
+    html += "<li>" + str(std.RollNo) + " " + str(std.Name) + "</li>"
+    html += "</ol>"
+    return HttpResponse(html)
+    '''
+    try:
+        std = Student.objects.get(RollNo=roll_no)
+        html = "<ol>"
+        html += "<li>" + str(std.RollNo) + " " + str(std.Name) + "</li>"
+        html += </ol>
+    except Exception:
+        raise Http404("Page Not Found")
+    return HttpResponse(html)
+    
     return HttpResponse(
-        roll_no + "<table border =  2 cellspacing = 1 cellpadding = 10 bgcolor = 'cyan' bordercolor = 'red'>"
-                  "<tr>"
-                  "<th>Roll No</th>"
-                  "<th>Name</th>"
-                  "<th>Class</th>"
-                  "</tr>"
-                  "<tr>"
-                  "<td>1</td>"
-                  "<td>Rohit Singla</td>"
-                  "<td>BCA</td>"
-                  "</tr>"
-                  "<tr>"
-                  "<td>2.</td>"
-                  "<td>Harman</td>"
-                  "<td>B.Tech</td>"
-                  "</tr>"
-                  "</table>"
-                  "<br/>"
-                  "</br>"
-                  "<ol>"
-                  "<li>Rohit</a></li>"
-                  "<li>Dinesh</li>"
-                  "<li>Pranav</li>"
-                  "<li>Harman</li>"
-                  "</ol>")
+       roll_no + "<table border =  2 cellspacing = 1 cellpadding = 10 bgcolor = 'cyan' bordercolor = 'red'>"
+                 "<tr>"
+                 "<th>Roll No</th>"
+                 "<th>Name</th>"
+                 "<th>Class</th>"
+                 "</tr>"
+                 "<tr>"
+                 "<td>1</td>"
+                 "<td>Rohit Singla</td>"
+                 "<td>BCA</td>"
+                 "</tr>"
+                 "<tr>"
+                 "<td>2.</td>"
+                 "<td>Harman</td>"
+                 "<td>B.Tech</td>"
+                 "</tr>"
+                 "</table>"
+                 "<br/>"
+                 "</br>"
+                 "<ol>"
+                 "<li>Rohit</a></li>"
+                 "<li>Dinesh</li>"
+                 "<li>Pranav</li>"
+                 "<li>Harman</li>"
+                 "</ol>")
+   '''
