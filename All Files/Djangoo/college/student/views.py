72215ca61954debb57cf8ed92b9f90671bcd7d8e
Rohit Singla <rohitsingla2611@gmail.com>
2018-07-16 09:08:09 +0530

    Update

--- All Files/Djangoo/college/student/views.py
+++ All Files/Djangoo/college/student/views.py
@@ -1,5 +1,5 @@
 # Create your views here.
-
+from django.contrib import messages
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.http import HttpResponse, Http404
 
