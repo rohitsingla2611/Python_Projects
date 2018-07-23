ee01d90c50ac2fd01896b55ccfa887aae36691a2
Rohit Singla <rohitsingla2611@gmail.com>
2018-07-23 16:59:56 +0530

    Update

---
+++ All Files/OpenCv/OpenCv2.py
@@ -0,0 +1,43 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+
+def make_1080p():
+    cap.set(3, 1920)
+    cap.set(4, 1080)
+
+
+def make_720p():
+    cap.set(3, 1280)
+    cap.set(4, 720)
+
+
+def make_480p():
+    cap.set(3, 640)
+    cap.set(4, 480)
+
+
+def change_res(width, height):
+    cap.set(3, width)
+    cap.set(4, height)
+
+
+# make_1080p()
+# make_480p()
+# make_720p()
+change_res(4000, 2000)
+
+while True:
+    ret, frame = cap.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('frame', frame)
+    cv2.imshow('GRAY', gray)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
